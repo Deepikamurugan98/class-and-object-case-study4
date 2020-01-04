f9c3ef6dd797b10ec227387549ee6a229e8db764
Deepikamurugan98 <59245852+Deepikamurugan98@users.noreply.github.com>
2020-01-04 15:13:46 +0530

    Add files via upload

---
+++ abstraction1/src/abstraction1/Planet.java
@@ -0,0 +1,16 @@
+package abstraction1;
+
+public class Planet {
+	protected String planetName;
+	public Planet(String planetName) {
+	super();
+	this.planetName = planetName;
+	}
+	public String getPlanetName() {
+	return planetName;
+	}
+	public void setPlanetName(String planetName) {
+	this.planetName = planetName;
+	}
+
+}
